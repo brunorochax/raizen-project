fd8dc32f632806777627422c49f4f49c365c5e76
Bruno Rocha <bruno.sousax@gmail.com>
2021-07-12 18:03:05 -0300

    :recycle: Refining files organization

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eou pipefail
 
+echo "Starting vagrant environment.."
 chown root:kvm /dev/kvm
 service libvirtd start
 service virtlogd start
@@ -15,11 +16,17 @@ iptables -P INPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables -P OUTPUT ACCEPT
 
+virtual_host=$(vagrant ssh-config | grep HostName)
+virtual_ip=${virtual_host//"HostName "/""}
+virtual_ip=${virtual_ip//[[:space:]]/}
+
 iptables -A FORWARD -i eth0 -o virbr1 -p tcp --syn --dport 3389 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -i eth0 -o virbr1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -i virbr1 -o eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 3389 -j DNAT --to-destination 192.168.121.68
-iptables -t nat -A POSTROUTING -o virbr1 -p tcp --dport 3389 -d 192.168.121.68 -j SNAT --to-source 192.168.121.1
+
+echo "Setting virtual machine IP $virtual_ip for PREROUTING and POSTROUTING.."
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 3389 -j DNAT --to-destination $virtual_ip
+iptables -t nat -A POSTROUTING -o virbr1 -p tcp --dport 3389 -d $virtual_ip -j SNAT --to-source 192.168.121.1
 
 iptables -D FORWARD -o virbr1 -j REJECT --reject-with icmp-port-unreachable
 iptables -D FORWARD -i virbr1 -j REJECT --reject-with icmp-port-unreachable
